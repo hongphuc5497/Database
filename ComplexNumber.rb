994431df9c89cd6ad88a1e923be5c35deeb1ce1e
HongPhuc <hongphuc.dthp@gmail.com>
2019-06-19 08:18:05 +0700

    Fix my code

--- ComplexNumber.rb
+++ ComplexNumber.rb
@@ -6,60 +6,100 @@ class ComplexNumber
         @imaginary = imaginary
     end
 
+    # def self.input
+    #     print "Pls input real number : "
+    #     real = gets.chomp
+    #     @real = real.to_i
+    #     print "Pls input imaginary number : "
+    #     imaginary = gets.chomp
+    #     @imaginary = imaginary.to_i
+    #     complex = ComplexNumber.new(@real,@imaginary)
+    # end
 
-    def self.input
-        print "Pls input real number : "
-        real = gets.chomp
-        @real = real.to_i
-        print "Pls input imaginary number : "
-        imaginary = gets.chomp
-        @imaginary = imaginary.to_i
-        complex = ComplexNumber.new(@real,@imaginary)
+    def output
+        "#{self.real} + #{self.imaginary}i"
     end
+    # def self.output(complex)
+    #     puts "#{complex.real} + #{complex.imaginary}i"
+    # end
 
-    def self.output(complex)
-        puts "#{complex.real} + #{complex.imaginary}i"
-    end
-
-    def sum(t)
-        @real += t.real
-        @imaginary += t.imaginary
+    def +(t)
+        self.real += t.real
+        self.imaginary += t.imaginary
         self
     end
+    # def sum(t)
+    #     @real += t.real
+    #     @imaginary += t.imaginary
+    #     self
+    # end
 
-    def subtract(t)
-        @real -= t.real
-        @imaginary -= t.imaginary
+    def -(t)
+        self.real -= t.real
+        self.imaginary -= t.imaginary
         self
     end
+    # def subtract(t)
+    #     @real -= t.real
+    #     @imaginary -= t.imaginary
+    #     self
+    # end
 
-    def multiply(t)
-        @real = (@real * t.real) - (@imaginary * t.imaginary)
-        @imaginary= (@real * t.imaginary) + (@imaginary * t.real)
+    def *(t)
+        self.real = (self.real * t.real) - (self.imaginary * t.imaginary)
+        self.imaginary = (self.real * t.imaginary) - (self.imaginary * t.real)
         self
     end
+    # def multiply(t)
+    #     @real = (@real * t.real) - (@imaginary * t.imaginary)
+    #     @imaginary = (@real * t.imaginary) - (@imaginary * t.real)
+    #     self
+    # end
 
-    def divide(t)
-        @real = ((@real * t.real) + (@imaginary + t.imaginary))/(t.real*t.real) + (t.imaginary*t.imaginary)
-        @imaginary = ((@imaginary * t.real) - (@real * t.imaginary))/(t.real*t.real) + (t.imaginary*t.imaginary)
+    def /(t)
+        self.real = ((self.real * t.real) + (self.imaginary + t.imaginary))/(t.real*t.real) + (t.imaginary*t.imaginary)
+        self.imaginary = ((self.imaginary * t.real) - (self.real * t.imaginary))/(t.real*t.real) + (t.imaginary*t.imaginary)
         self
-    end   
+    end
+    # def divide(t)
+    #     @real = ((@real * t.real) + (@imaginary + t.imaginary))/(t.real*t.real) + (t.imaginary*t.imaginary)
+    #     @imaginary = ((@imaginary * t.real) - (@real * t.imaginary))/(t.real*t.real) + (t.imaginary*t.imaginary)
+    #     self
+    # end   
 end
 
 
-a = ComplexNumber.input
-b = ComplexNumber.input
 
+
+
+# a = ComplexNumber.input
+# b = ComplexNumber.input
+a = ComplexNumber.new(3, 9)
+b = ComplexNumber.new(5, 15)
 print "Complex number : " 
-ComplexNumber.output(a) 
+#ComplexNumber.output(a) 
+puts a.output
+puts b.output
+
 print "Sum : "
-ComplexNumber.output(a.sum(b))
+# ComplexNumber.output(a.sum(b))
+c = a.clone 
+puts (c+b).output
+
 print "Subtract : "
-ComplexNumber.output(a.subtract(b))
+# ComplexNumber.output(a.subtract(b))
+d = a.clone
+puts (d-b).output
+
 print "Multiply : "
-ComplexNumber.output(a.multiply(b))
+# ComplexNumber.output(a.multiply(b))
+e = a.clone
+puts (e*a).output
+
 print "Divide : "
-ComplexNumber.output(a.divide(b))
+# ComplexNumber.output(a.divide(b))
+f = a.clone
+puts (f/b).output
 
 
 
